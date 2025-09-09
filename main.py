c0976f375f870a566eddf49462fc285b8f5856e9
Fernabim <109820905+FernandaNav@users.noreply.github.com>
2025-09-09 11:02:09 -0600

    Primer intento
    Interfaz básica solo para probar funcionamiento

--- main.py
+++ main.py
@@ -1,39 +1,49 @@
-# main.py
-import os
-from logic import procesar_regex
+import re
+import tkinter as tk
+from tkinter import messagebox
 
-def Clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
+def aplicar_regex():
+    regex = entrada_regex.get()
+    texto = entrada_texto.get("1.0", tk.END)
 
-def Continue():
-    input("\nPresiona ENTER para continuar...")
-    Clear()
+    try:
+        patron = re.compile(regex)
+    except re.error as e:
+        messagebox.showerror("Error", f"Regex inválida:\n{e}")
+        return
 
-def main():
-    Clear()
-    while True:
-        print("----------BIENVENIDO\n")
-        print("  1. Ingresar expresión.")
-        print("  2. Salir.\n")
-        try:
-            option = int(input("Ingresa una opción: "))
-        except ValueError:
-            print(" Opción inválida. Ingresa un número.")
-            Continue()
-            continue
+    coincidencias = patron.findall(texto)
 
-        match option:
-            case 1:
-                Clear()
-                print("----INGRESAR EXPRESIÓN\n")
-                procesar_regex()
-                Continue()
-            case 2:
-                print("\n----Adiós")
-                break
-            case _:
-                print("⚠️ Opción no existe.")
-                Continue()
+    salida_resultados.delete("1.0", tk.END)
+    salida_resultados.insert(tk.END, "Coincidencias:\n")
+    for c in coincidencias:
+        salida_resultados.insert(tk.END, f"- {c}\n")
 
-if __name__ == "__main__":
-    main()
+    entrada_texto.tag_remove("resaltado", "1.0", tk.END)
+    for match in patron.finditer(texto):
+        start, end = match.span()
+        start_index = f"1.0+{start}c"
+        end_index = f"1.0+{end}c"
+        entrada_texto.tag_add("resaltado", start_index, end_index)
+
+    entrada_texto.tag_config("resaltado", background="yellow")
+
+ventana = tk.Tk()
+ventana.title("Herramienta Regex")
+
+tk.Label(ventana, text="Expresión Regular:").pack()
+entrada_regex = tk.Entry(ventana, width=50)
+entrada_regex.pack()
+
+##entrada de texto
+tk.Label(ventana, text="Texto:").pack()
+entrada_texto = tk.Text(ventana, width=60, height=10)
+entrada_texto.pack()
+tk.Button(ventana, text="Aplicar Regex", command=aplicar_regex).pack(pady=5)
+
+##resultados 
+tk.Label(ventana, text="Resultados:").pack()
+salida_resultados = tk.Text(ventana, width=60, height=5)
+salida_resultados.pack()
+
+ventana.mainloop()
